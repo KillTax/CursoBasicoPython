2db4d5f32f48380f3f3de89c339a38bfc8d68f81
killtax <maxvega091@gmail.com>
2021-11-04 16:21:13 -0600

    se agregan dos nuevos archivos sobre operadores y variables

---
+++ operadores_aritmeticos.py
@@ -0,0 +1,24 @@
+import math
+
+#suma
+1 + 1
+
+#resta
+1 - 1
+
+#division
+10/2
+
+#residuo de la division
+11%2
+
+#multiplicacion
+10*2
+
+#potencia
+10**2
+
+#raiz tiene 2 formas
+9**0.5
+math.sqrt(9)
+
---
+++ variable.py
@@ -0,0 +1,15 @@
+#Las variables en python se pueden crear de una forma simple
+#solo tienes que poner el nombre de la variable y asignarle un valor
+#no es necesario darle un tipo de dato
+
+numero1 = 3
+
+numero2 = 5
+
+#Ahora puedes modificar estos datos
+
+numero1 = 2
+
+#Puedes crear variables de con otras variables y operar con ellas
+numero_suma = numero1 + numero2
+
