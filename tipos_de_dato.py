f00d67851a139dacd8ab57aafd33e52d66af656c
killtax <maxvega091@gmail.com>
2021-11-10 16:40:46 -0600

    Se agrega un pequeno ejercicio y explicacion sobre tipos de datos

---
+++ tipos_de_dato.py
@@ -0,0 +1,26 @@
+#Podemos poner un string en una variable, este puede ir tanto en comillas simples '' como en comillas dobles ""
+nombre = "Max"
+nombre2 = 'Maximino'
+print(nombre + nombre2)
+
+#Cunado sumamos textos estamos concatenando los textos, podemos operar con cadenas de caracteres
+print(nombre * 4)
+
+#Podemos dar un espaciado en las cadenas de texto concatendas de las siguiente manera
+print(nombre + ", " + nombre2)
+
+#Tambien podemos crear
+numero_decimal = 3.4
+print(numero_decimal)
+
+#Hay que recordar que python se rige por el idioma ingles entonces siempre hay que utilizar el "."para usar
+#los decimales
+
+#Tambien existen los booleanos que son verdadero o falso (True o False)
+es_estudiante = True
+print(es_estudiante)
+
+trabaja = False
+print(trabaja)
+
+#Estos nos pueden servir para confirmar cierto tipo de informacion mediante variables
